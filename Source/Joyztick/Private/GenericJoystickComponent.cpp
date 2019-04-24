e11d3876db3cd46bce36412b909de23313ff41d4
Luiz Fernando <fernandoaff1@gmail.com>
2019-04-24 08:12:40 -0300

    Update default value of bJoyztickInputEnabled

--- Source/Joyztick/Private/GenericJoystickComponent.cpp
+++ Source/Joyztick/Private/GenericJoystickComponent.cpp
@@ -30,6 +30,7 @@ UGenericJoystickComponent::UGenericJoystickComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	bJoyztickInputEnabled = true;
 
 	// ...
 }
